bd59cd21d29b709fd82dd3e4b12f34f2cca8c0d7
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-09-23 15:10:35 +0100

    Resumés: structures revisions and template addtion

---
+++ app/components/yr-templates/yr-template-2.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import ConstantsMixin from 'you-rockstar/mixins/constants';
+import TemplateStateMixin from 'you-rockstar/mixins/template/state';
+import UserMixin from 'you-rockstar/mixins/data/user';
+
+export default Ember.Component.extend(
+  ConstantsMixin,
+  TemplateStateMixin,
+  UserMixin
+);
--- app/mixins/data/palettes.js
+++ app/mixins/data/palettes.js
@@ -4,132 +4,63 @@ export default Ember.Mixin.create({
   palettesData: Ember.A([
     Ember.Object.create({
       id: 1,
-      colors: [
-        { hex: '#bbdefb', is_light: true },
-        { hex: '#212529', is_light: false },
-        { hex: '#585858', is_light: false }
-      ],
-      is_selected: false,
-      template_id: 1,
-      is_light_bg: false
+      is_selected: true,
+      template_id: 1
     }),
     Ember.Object.create({
       id: 2,
-      colors: [
-        { hex: '#673AB7', is_light: false },
-        { hex: '#fff', is_light: true },
-        { hex: '#212529', is_light: false }
-      ],
       is_selected: false,
-      template_id: 1,
-      is_light_bg: true
+      template_id: 1
     }),
-    // Ember.Object.create({
-    //   id: 3,
-    //   colors: [
-    //     { hex: '#123f61', is_light: false },
-    //     { hex: '#1e6291', is_light: true },
-    //     { hex: '#66a4c0', is_light: false },
-    //     { hex: '#99ccff', is_light: false },
-    //     { hex: '#000', is_light: false }
-    //   ],
-    //   is_selected: false,
-    //   template_id: 2,
-    //   is_light_bg: true
-    // }),
-    // Ember.Object.create({
-    //   id: 4,
-    //   colors: [
-    //     { hex: '#fff8f8', is_light: false },
-    //     { hex: '#e9cbca', is_light: false },
-    //     { hex: '#daa8a6', is_light: false },
-    //     { hex: '#940000', is_light: false },
-    //     { hex: '#580000', is_light: false }
-    //   ],
-    //   is_selected: false,
-    //   template_id: 2,
-    //   is_light_bg: true
-    // }),
-    // Ember.Object.create({
-    //   id: 5,
-    //   colors: [
-    //     { hex: '#34a853', is_light: false },
-    //     { hex: '#4285f4', is_light: false },
-    //     { hex: '#ed1c24', is_light: false },
-    //     { hex: '#fbbc05', is_light: false },
-    //     { hex: '#a5a0a9', is_light: false }
-    //   ],
-    //   is_selected: false,
-    //   template_id: 3,
-    //   is_light_bg: true
-    // }),
-    // Ember.Object.create({
-    //   id: 6,
-    //   colors: [
-    //     { hex: '#34a853', is_light: false },
-    //     { hex: '#4285f4', is_light: false },
-    //     { hex: '#ed1c24', is_light: false },
-    //     { hex: '#fbbc05', is_light: false },
-    //     { hex: '#a5a0a9', is_light: false }
-    //   ],
-    //   is_selected: false,
-    //   template_id: 3,
-    //   is_light_bg: true
-    // }),
-    // Ember.Object.create({
-    //   id: 7,
-    //   colors: [
-    //     { hex: '#a4281c', is_light: false },
-    //     { hex: '#dd4c2f', is_light: false },
-    //     { hex: '#f68448', is_light: false },
-    //     { hex: '#f89c47', is_light: false },
-    //     { hex: '#f7c5a4', is_light: false }
-    //   ],
-    //   is_selected: false,
-    //   template_id: 3,
-    //   is_light_bg: true
-    // }),
+    Ember.Object.create({
+      id: 3,
+      is_selected: true,
+      template_id: 2
+    }),
+    Ember.Object.create({
+      id: 4,
+      is_selected: false,
+      template_id: 2
+    }),
+    Ember.Object.create({
+      id: 5,
+      is_selected: false,
+      template_id: 3
+    }),
+    Ember.Object.create({
+      id: 6,
+      is_selected: false,
+      template_id: 3
+    }),
+    Ember.Object.create({
+      id: 7,
+      is_selected: false,
+      template_id: 3
+    }),
     Ember.Object.create({
       id: 8,
-      colors: [
-        { hex: '#3f51b5', is_light: false },
-        { hex: '#fff', is_light: true },
-        { hex: '#212529', is_light: false }
-      ],
       is_selected: false,
-      template_id: 1,
-      is_light_bg: true
-    }),
-    Ember.Object.create({
-      id: 9,
-      colors: [
-        { hex: '#e91e63', is_light: false },
-        { hex: '#fff', is_light: true },
-        { hex: '#212529', is_light: false }
-      ],
-      is_selected: false,
-      template_id: 1,
-      is_light_bg: true
+      template_id: 1
     }),
     Ember.Object.create({
       id: 10,
-      colors: [
-        { hex: '#673AB7', is_light: false },
-        { hex: '#fff', is_light: true }
-      ],
       is_selected: false,
-      template_id: 1,
-      is_light_bg: false
+      template_id: 1
     }),
     Ember.Object.create({
-      id: 11,
-      colors: [
-        { hex: '#212529', is_light: false },
-        { hex: '#fff', is_light: true }
-      ],
-      is_selected: true,
-      template_id: 1,
-      is_light_bg: true
+      id: 12,
+      is_selected: false,
+      template_id: 2
+    }),
+    Ember.Object.create({
+      id: 13,
+      is_selected: false,
+      template_id: 2
+    }),
+    Ember.Object.create({
+      id: 14,
+      is_selected: false,
+      template_id: 2
     })
   ])
 });
