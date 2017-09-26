399cb81a7c0240bced101b58fe2fac5fb7b6d42c
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-09-26 19:24:37 +0100

    Get Started: fixing image paths implementation (#2)

--- app/components/yr-get-started.js
+++ app/components/yr-get-started.js
@@ -1,12 +1,5 @@
 import Ember from 'ember';
-import ConstantsMixin from 'you-rockstar/mixins/constants';
 
-export default Ember.Component.extend(ConstantsMixin, {
-  classNames: 'full-height',
-  device: Ember.computed('[]', function() {
-    return this.get('PATH_TO_IMAGES') + 'homepage/imac-1.png';
-  }),
-  behindDevice: Ember.computed('[]', function() {
-    return this.get('PATH_TO_IMAGES') + 'preview/screen-shot.png';
-  })
+export default Ember.Component.extend({
+  classNames: 'full-height'
 });
