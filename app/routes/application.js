baaa0cecfa4fb2b0dcf108ee2c01c4969500fe70
Iorrah Gonzalez <iorrah.gonzalez@gmail.com>
2017-10-05 14:30:00 +0200

    General: structure improvements

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   title: function(tokens) {
     var title = '';
 
-    if (tokens[0].indexOf('_') == 0) {
+    if (tokens[0].indexOf('_') === 0) {
       title = 'You Rockstar • ' + tokens[0].substr(1);
     } else {
       title = tokens.join(' • ');
